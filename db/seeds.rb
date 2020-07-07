04dd6f2597d1ef15207fb16f1c1f13d6f56368b6
CMSTie <cmstie@gmail.com>
2020-07-06 20:53:17 -0400

    db mostly working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Game.destroy_all
+
+require "uri"
+require "net/http"
+
+# url = URI("https://api.rawg.io/api/games")
+
+# https = Net::HTTP.new(url.host, url.port);
+# https.use_ssl = true
+
+# request = Net::HTTP::Get.new(url)
+# request["Authorization"] = "token 427e5b91ccbed6e172681d8018b2fa2777c9739f"
+# request["Content-Type"] = "text/plain"
+# request["Cookie"] = "__cfduid=d3d33ed140d75bab15dca39b361c406ff1594060379"
+# request.body = "{\"title\": \"My issue Title\", \"body\": \"what a large issue\"}\n"
+
+# response = https.request(request)
+# games = response.read_body
+
+info = URI.parse("https://api.rawg.io/api/games")
+response = Net::HTTP.get_response(info)
+        game_info = JSON.parse(response.body)
+
+            game_info["results"].each do |game|
+            
+            name = game["name"]
+            rating = game["rating"]
+            genre = game["genres"][0]['name']
+
+            # platform = game["parent_platforms"][0]["platform"]["name"]
+            platform = game["parent_platforms"][1]["platform"]["name"]
+            img_url = game["background_image"]
+            link = game["stores"][0]["url_en"]
+            Game.create(name: name, rating: rating, img_ur: img_url, platform: platform, link: link, genre: genre)
+            end
+        
