d99413bc817cb919850d48d25f932666003decf9
CMSTie <cmstie@gmail.com>
2020-07-06 21:41:20 -0400

    added model relationships

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+    has_many :usergames
+    has_many :users, through: :usergame
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :usergames
+    has_many :games, through: :usergames
 end
