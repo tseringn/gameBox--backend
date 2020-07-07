5f02106d73207e5393e384cbb1dfe01a3d856786
callas4ever <thetseringnorbu@gmail.com>
2020-07-07 18:53:06 -0400

    Deleted Front End folder

--- frontEnd/js/index.js
+++
@@ -1,6 +0,0 @@
-const userUrl='http://localhost:3000/api/v1/users'
-const userUrl='http://localhost:3000/api/v1/games'
-const userUrl='http://localhost:3000/api/v1/user_games'
-function fetchGame(){
-    fetch()
-}
