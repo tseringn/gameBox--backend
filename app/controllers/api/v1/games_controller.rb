5c362b32ef99d7491897cd1b9ad2fc2c76aa8a76
callas4ever <thetseringnorbu@gmail.com>
2020-07-08 12:30:32 -0400

    added user show controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::GamesController < ApplicationController
     games=Game.all
     render json: games
     end
+    def show
+    game=Game.find(params[:id])
+    render json: game
+    end
 end
